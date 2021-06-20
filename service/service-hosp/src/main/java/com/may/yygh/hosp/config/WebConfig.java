1c52e373efa4e53d7a76ed238245df4a1f5d6860
mayongfeng <272691958@qq.com>
2021-06-20 10:33:50 +0800

    增加服务调用

--- service/service-hosp/src/main/java/com/may/yygh/hosp/config/WebConfig.java
+++ service/service-hosp/src/main/java/com/may/yygh/hosp/config/WebConfig.java
@@ -8,10 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     static final String ORIGINS[] = new String[]{"GET", "POST", "PUT", "DELETE"};
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowCredentials(true).allowedMethods(ORIGINS)
-                .maxAge(3600);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**").allowedOrigins("*").allowCredentials(true).allowedMethods(ORIGINS)
+//                .maxAge(3600);
+//    }
 
 }
