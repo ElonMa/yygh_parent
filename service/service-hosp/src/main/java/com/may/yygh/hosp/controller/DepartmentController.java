88afd1a68c5400022c2556bcefa3c1457dc5f409
mayongfeng <272691958@qq.com>
2021-06-20 09:26:29 +0800

    增加预约挂号查询

---
+++ service/service-hosp/src/main/java/com/may/yygh/hosp/controller/DepartmentController.java
@@ -0,0 +1,28 @@
+package com.may.yygh.hosp.controller;
+
+import com.may.yygh.common.result.Result;
+import com.may.yygh.hosp.service.DepartmentService;
+import com.may.yygh.vo.hosp.DepartmentVo;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/hosp/department")
+public class DepartmentController {
+    @Autowired
+    private DepartmentService departmentService;
+
+    //根据医院编号，查询医院所有科室列表
+    @ApiOperation(value = "查询医院所有科室列表")
+    @GetMapping("getDeptList/{hoscode}")
+    public Result getDeptList(@PathVariable String hoscode) {
+        List<DepartmentVo> list = departmentService.findDeptTree(hoscode);
+        return Result.ok(list);
+    }
+}
